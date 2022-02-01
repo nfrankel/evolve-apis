dab34cd59c0137e518af80eca56632c196c32d6f
Nicolas Frankel <nicolas@frankel.ch>
2022-02-01 18:09:56 +0100

    Create the direct route from the gateway to the old API

---
+++ evolve/1_create_direct_route.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+docker run --network evolve-apis_default --rm curlimages/curl:7.81.0 -v -i http://apisix:9180/apisix/admin/routes/1 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+  "name": "Direct Route to Old API",
+  "uri": "/hello*",
+  "upstream": {
+    "nodes": {
+      "oldapi:8081": 1
+    }
+  },
+  "plugins": {
+    "prometheus": {}
+  }
+}'
