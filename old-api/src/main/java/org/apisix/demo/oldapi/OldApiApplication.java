4e61ebe7272a56d5db6d1b8813d9c79dfb394de2
Nicolas Frankel <nicolas@frankel.ch>
2022-02-01 08:54:10 +0100

    Initial API version

---
+++ old-api/src/main/java/org/apisix/demo/oldapi/OldApiApplication.java
@@ -0,0 +1,12 @@
+package org.apisix.demo.oldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldApiApplication.class, args);
+    }
+}
