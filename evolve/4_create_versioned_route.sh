61017fc19bfd26e3b068cd2813a274f065c3be31
Nicolas Frankel <nicolas@frankel.ch>
2022-02-02 10:24:21 +0100

    Create the v1 route to the old API

---
+++ evolve/4_create_versioned_route.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+curl http://localhost:9180/apisix/admin/plugin_configs/1 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+  "plugins": {
+    "proxy-rewrite": {
+      "regex_uri": ["/v1(.*)", "$1"]
+    }
+  }
+}'
+
+curl http://localhost:9180/apisix/admin/routes/2 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+  "name": "Versioned Route to Old API",
+  "uri": "/v1/hello*",
+  "upstream_id": 1,
+  "plugin_config_id": 1
+}'
+
