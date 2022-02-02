60e26ad7340d95d91406dba5370f3213944f454c
Nicolas Frankel <nicolas@frankel.ch>
2022-02-02 10:24:21 +0100

    Create the v1 route to the old API

---
+++ evolve/4_create_versioned_route.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+docker run --network evolve-apis_default --rm curlimages/curl:7.81.0 -v -i http://apisix:9080/apisix/admin/plugin_configs/1 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+  "plugins": {
+    "proxy-rewrite": {
+      "regex_uri": ["/v1/(.*)", "/$1"]
+    }
+  }
+}'
+
+docker run --network evolve-apis_default --rm curlimages/curl:7.81.0 -v -i http://apisix:9080/apisix/admin/routes/2 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+  "name": "Versioned Route to Old API",
+  "methods": ["GET"],
+  "uris": ["/v1/hello", "/v1/hello/", "/v1/hello/*"],
+  "upstream_id": 1,
+  "plugin_config_id": 1
+}'
+
