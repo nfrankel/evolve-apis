405c2654149b560e803d012bb6113f1b824b23f9
Nicolas Frankel <nicolas@frankel.ch>
2022-02-15 18:42:16 +0100

    Create the v2 route from the gateway to the New API

---
+++ evolve/10_create_v2_route.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+curl http://localhost:9180/apisix/admin/routes/3 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+  "name": "Versioned Route to New API",
+  "uri": "/v2/hello*",
+  "upstream_id": 2,
+  "plugins": {
+    "proxy-rewrite": {
+      "regex_uri": ["/v2(.*)", "$1"]
+    },
+    "unauth-limit": {
+      "count": 1,
+      "time_window": 60,
+      "key_type": "var",
+      "key": "consumer_name",
+      "rejected_code": 429,
+      "rejected_msg": "Please register at https://apisix.org/register to get your API token and enjoy unlimited calls"
+    }
+  }
+}'
