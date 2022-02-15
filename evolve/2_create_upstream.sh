9a36e5d6d66b19c5e7066d3e66616343e3c752a2
Nicolas Frankel <nicolas@frankel.ch>
2022-02-15 16:29:26 +0100

    Introduce dedicated upstream for Old API

---
+++ evolve/2_create_upstream.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+docker run --network evolve-apis_default --rm curlimages/curl:7.81.0 -v -i http://apisix:9180/apisix/admin/upstreams/1 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+  "name": "Old API",
+  "type": "roundrobin",
+  "nodes": {
+    "oldapi:8081": 1
+  }
+}'
